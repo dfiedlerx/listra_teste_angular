70037c8408a77f4a3c1abf5c110af78fe59cbbc6
Daniel Fiedler <danielfiedler_@hotmail.com>
2022-10-06 15:59:02 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'listra_teste_angular';
+}
